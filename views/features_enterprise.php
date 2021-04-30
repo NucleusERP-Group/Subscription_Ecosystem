30bdeb03c77abb8059a12ac4cb92b8dac8f16ec0
MartMbithi <martdevelopers254@gmail.com>
2021-04-30 16:18:24 +0300

    Crafted Features On Enterprise Version

--- views/features_enterprise.php
+++ views/features_enterprise.php
@@ -25,15 +25,76 @@
                     <div class="column-25">
                         <div class="feature-box">
                             <div class="feature-header">
-                                <i class="fas fa-shopping-cart icon-feature"></i>
+                                <i class="fas fa-file-alt icon-feature"></i>
                             </div>
                             <div class="feature-content">
                                 <h3>
-                                    Purchase
+                                    Online Accounting
                                 </h3>
                                 <p>
-                                    Simply start by purchasing one of our certificates offered above.
-                                </p>
+                                    See your financials online anytime, anywhere on your Mac, PC, tablet or mobile phone. No limits. </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="column-25">
+                        <div class="feature-box">
+                            <div class="feature-header">
+                                <i class="fas fa-piggy-bank icon-feature"></i>
+                            </div>
+                            <div class="feature-content">
+                                <h3>
+                                    Bank Accounts Intergration
+                                </h3>
+                                <p>
+                                    Create unlimited bank and cash accounts and track their opening and current balances.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="column-25">
+                        <div class="feature-box">
+                            <div class="feature-header">
+                                <i class="fas fa-money-check-alt icon-feature"></i>
+                            </div>
+                            <div class="feature-content">
+                                <h3>
+                                    Multi-Currency Support
+                                </h3>
+                                <p>
+                                    Send invoices and add expenses in any currency and let the system convert them in your main currency.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="column-25">
+                        <div class="feature-box">
+                            <div class="feature-header">
+                                <i class="fas fa-users icon-feature"></i>
+                            </div>
+                            <div class="feature-content">
+                                <h3>
+                                    Customer Management
+                                </h3>
+                                <p>
+                                    Create clients and send invoices to them. You can also set a password so they could to access the client portal. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column-row">
+                    <div class="column-25">
+                        <div class="feature-box">
+                            <div class="feature-header">
+                                <i class="fas fa-file-invoice icon-feature"></i>
+                            </div>
+                            <div class="feature-content">
+                                <h3>
+                                    Easy Invoicing
+                                </h3>
+                                <p>
+                                    Send professional invoices to clients and start accepting online payments, no commission/transaction fee. </p>
                             </div>
                         </div>
                     </div>
@@ -44,40 +105,143 @@
                             </div>
                             <div class="feature-content">
                                 <h3>
-                                    Activate
+                                    Deposits & Transfers
                                 </h3>
                                 <p>
-                                    Follow the given validation instructions to activate your new certificate.
-                                </p>
+                                    Add deposits to and transfers between accounts and keep the balance of your bank accounts active.
                             </div>
                         </div>
                     </div>
                     <div class="column-25">
                         <div class="feature-box">
                             <div class="feature-header">
-                                <i class="fas fa-cog icon-feature"></i>
+                                <i class="fas fa-user-tie icon-feature"></i>
                             </div>
                             <div class="feature-content">
                                 <h3>
-                                    Install
+                                    Vendor Management
                                 </h3>
                                 <p>
-                                    You'll receive installation instructions as soon as your certificate has been issued.
-                                </p>
+                                    Create vendors so you could assign bills and payments to them and later filter their transactions easily. </p>
                             </div>
                         </div>
                     </div>
                     <div class="column-25">
                         <div class="feature-box">
                             <div class="feature-header">
-                                <i class="fas fa-sliders-h icon-feature"></i>
+                                <i class="fas fa-shopping-cart icon-feature"></i>
                             </div>
                             <div class="feature-content">
                                 <h3>
-                                    Manage
+                                    Billable Expenses
                                 </h3>
                                 <p>
-                                    You can easily manage and reissue all your certificates in our customer portal.
+                                    Create clients and send invoices to them. You can also set a password so they could to access the client portal. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column-row">
+                    <div class="column-25">
+                        <div class="feature-box">
+                            <div class="feature-header">
+                                <i class="fas fa-credit-card icon-feature"></i>
+                            </div>
+                            <div class="feature-content">
+                                <h3>
+                                    Various Payments
+                                </h3>
+                                <p>
+                                    Send professional invoices to clients and start accepting online payments, no commission/transaction fee. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column-25">
+                        <div class="feature-box">
+                            <div class="feature-header">
+                                <i class="fas fa-address-book icon-feature"></i>
+                            </div>
+                            <div class="feature-content">
+                                <h3>
+                                    Taxes Management
+                                </h3>
+                                <p>
+                                    Add deposits to and transfers between accounts and keep the balance of your bank accounts active.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column-25">
+                        <div class="feature-box">
+                            <div class="feature-header">
+                                <i class="fas fa-chart-line icon-feature"></i>
+                            </div>
+                            <div class="feature-content">
+                                <h3>
+                                    Powerful Reporting
+                                </h3>
+                                <p>
+                                    Create vendors so you could assign bills and payments to them and later filter their transactions easily. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column-25">
+                        <div class="feature-box">
+                            <div class="feature-header">
+                                <i class="fas fa-user-check icon-feature"></i>
+                            </div>
+                            <div class="feature-content">
+                                <h3>
+                                    Customer Summary
+                                </h3>
+                                <p>
+                                    Create clients and send invoices to them. You can also set a password so they could to access the client portal. </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="column-row">
+                    <div class="column-25">
+                        <div class="feature-box">
+                            <div class="feature-header">
+                                <i class="fas fa-building icon-feature"></i>
+                            </div>
+                            <div class="feature-content">
+                                <h3>
+                                    Multi-Company
+                                </h3>
+                                <p>
+                                    Manage the finances of multiple companies from one admin panel. Assign users to different companies.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column-25">
+                        <div class="feature-box">
+                            <div class="feature-header">
+                                <i class="fas fa-user-shield icon-feature"></i>
+                            </div>
+                            <div class="feature-content">
+                                <h3>
+                                    Client Portal
+                                </h3>
+                                <p>
+                                    Share the transactions and invoices with your clients and accept bulk payments, online.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column-25">
+                        <div class="feature-box">
+                            <div class="feature-header">
+                                <i class="fas fa-check icon-feature"></i>
+                            </div>
+                            <div class="feature-content">
+                                <h3>
+                                    And Many More
+                                </h3>
+                                <p>
+                                    Grab NucleusSaaS ERP Enterprise Version and exploit many more features.
                                 </p>
                             </div>
                         </div>

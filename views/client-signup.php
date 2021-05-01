210b1dd644adc3b023df1133256c002fa7c5a441
MartMbithi <martdevelopers254@gmail.com>
2021-05-01 13:56:35 +0300

    Client Fix

--- views/client-signup.php
+++ views/client-signup.php
@@ -1,25 +1,46 @@
 <?php
+/*
+ * Created on Sat May 01 2021
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2021 MartDevelopers Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 session_start();
 include('../config/config.php');
 include('../config/codeGen.php');
 
-if (isset($_POST['Sign_Up'])) {
+if (isset($_POST['Sign Up'])) {
     //Error Handling and prevention of posting double entries
     $error = 0;
 
-    if (isset($_POST['Client_id']) && !empty($_POST['Client_id'])) {
-        $Client_id = mysqli_real_escape_string($mysqli, trim($_POST['Client_id']));
+    if (isset($_POST['id']) && !empty($_POST['id'])) {
+        $id = mysqli_real_escape_string($mysqli, trim($_POST['id']));
     } else {
         $error = 1;
         $err = "Client ID Cannot Be Empty";
     }
-    if (isset($_POST['Client_name']) && !empty($_POST['Client_name'])) {
-        $Client_name = mysqli_real_escape_string($mysqli, trim($_POST['Client_name']));
+    if (isset($_POST['name']) && !empty($_POST['name'])) {
+        $name = mysqli_real_escape_string($mysqli, trim($_POST['name']));
     } else {
         $error = 1;
-        $err = "Client Name Cannot Be Empty";
+        $err = "Name Cannot Be Empty";
     }
-    if (isset($_POST['Client_phone_no']) && !empty($_POST['Client_phone_no'])) {
+    if (isset($_POST['']) && !empty($_POST['Client_phone_no'])) {
         $Client_phone_no = mysqli_real_escape_string($mysqli, trim($_POST['Client_phone_no']));
     } else {
         $error = 1;

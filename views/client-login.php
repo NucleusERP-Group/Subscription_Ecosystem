203b91af4e063d292b905520332387a1e551cf89
MartMbithi <martdevelopers254@gmail.com>
2021-05-03 19:55:52 +0300

    Auth Logic

--- views/client-login.php
+++ views/client-login.php
@@ -1,6 +1,57 @@
-<?php 
+<?php
+/*
+ * Created on Mon May 03 2021
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2021 MartDevelopers Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+session_start();
+include('configs/config.php');
 
-require_once('../partials/_head.php'); 
+if (isset($_POST['login'])) {
+    /* Secure Login */
+    $error = 0;
+    if (isset($_POST['email']) && !empty($_POST['email'])) {
+        $email = mysqli_real_escape_string($mysqli, trim($_POST['email']));
+    } else {
+        $error = 1;
+        $err = "Email Cannot  Be Empty";
+    }
+    if (isset($_POST['password']) && !empty($_POST['password'])) {
+        $password = mysqli_real_escape_string($mysqli, trim(sha1(md5($_POST['password']))));
+    } else {
+        $error = 1;
+        $err = "Password Cannot  Be Empty";
+    }
+    if (!$error) {
+        $stmt = $mysqli->prepare("SELECT email, password, id  FROM iBursary_admin  WHERE email =? AND password =?");
+        $stmt->bind_param('ss', $email, $password); //bind fetched parameters
+        $stmt->execute(); //execute bind
+        $stmt->bind_result($email, $password, $id); //bind result
+        $rs = $stmt->fetch();
+        $_SESSION['id'] = $id;
+        if ($rs) {
+            header("location:dashboard.php");
+        } else {
+            $err =  "Access Denied, Incorrect Email Or Password";
+        }
+    }
+}
+require_once('../partials/_head.php');
 
 ?>
 

55b0aeb4d50af0ad5888ffb009df920b81d2adf2
MartMbithi <martdevelopers254@gmail.com>
2021-05-05 16:33:15 +0300

    Refactored New Dashboard Theme Assets

--- partials/_dashboard_footer.php
+++
@@ -1,5 +0,0 @@
-<!-- Core Dashboard Js -->
-<script src="../public/js/purpose.js"></script>
-<script src="../public/js/purpose.core.js"></script>
-<!-- Sweet Alert Js -->
-<script src="../public/js/swal.js"></script>

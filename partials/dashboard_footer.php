c4a6573e00c2c4af474643b0c1953cbc2be43ae2
MartMbithi <martdevelopers254@gmail.com>
2021-05-08 15:21:43 +0300

    Added Credit Card Details

--- partials/dashboard_footer.php
+++ partials/dashboard_footer.php
@@ -1,20 +1,7 @@
 <div class="footer pt-5 pb-4 footer-light" id="footer-main">
     <div class="row text-center text-sm-left align-items-sm-center">
-        <div class="col-sm-6">
+        <div class="col-sm-12">
             <p class="text-sm mb-0">&copy; 2019 - <?php echo date('Y'); ?>. NucleusSaaS ERP Group. A <a href="https://martdev.info/" target="_blank" class="h6 text-sm">MartDevelopers Inc</a> Production. All rights reserved.</p>
-        </div>
-        <div class="col-sm-6 mb-md-0">
-            <ul class="nav justify-content-center justify-content-md-end">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Support</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Terms</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link pr-0" href="#">Privacy</a>
-                </li>
-            </ul>
         </div>
     </div>
 </div>

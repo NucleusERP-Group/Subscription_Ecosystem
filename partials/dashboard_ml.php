cb9be5f0b25fef0e08839710789ee892217136c2
MartMbithi <martdevelopers254@gmail.com>
2021-05-08 12:15:21 +0300

    Minor Fixes

---
+++ partials/dashboard_ml.php
@@ -0,0 +1,8 @@
+<div class="page-title">
+    <div class="row justify-content-between align-items-center">
+        <div class="col-md-6 mb-3 mb-md-0">
+            <h5 class="h3 font-weight-400 mb-0 text-white">Moning, Heather!</h5>
+            <span class="text-sm text-white opacity-8">Have a nice day!</span>
+        </div>
+    </div>
+</div>
---
+++ partials/dashboard_sidenav.php
@@ -0,0 +1,60 @@
+<div class="sidenav" id="sidenav-main">
+    <!-- Sidenav header -->
+    <div class="sidenav-header d-flex align-items-center">
+        <a class="navbar-brand" href="client-dashboard.php">
+            <img src="../public/img/logos/header-light.png" class="navbar-brand-img" alt="...">
+        </a>
+        <div class="ml-auto">
+            <!-- Sidenav toggler -->
+            <div class="sidenav-toggler sidenav-toggler-dark d-md-none" data-action="sidenav-unpin" data-target="#sidenav-main">
+                <div class="sidenav-toggler-inner">
+                    <i class="sidenav-toggler-line bg-white"></i>
+                    <i class="sidenav-toggler-line bg-white"></i>
+                    <i class="sidenav-toggler-line bg-white"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- User mini profile -->
+    <div class="sidenav-user d-flex flex-column align-items-center justify-content-between text-center">
+        <!-- Avatar -->
+        <div>
+            <a href="#" class="avatar rounded-circle avatar-xl">
+                <img alt="Image placeholder" src="../public/assets/img/theme/light/team-1-800x800.jpg" class="">
+            </a>
+            <div class="mt-4">
+                <h5 class="mb-0 text-white">Heather Parker</h5>
+                <span class="d-block text-sm text-white opacity-8 mb-3">Web Architect</span>
+            </div>
+        </div>
+
+    </div>
+    <!-- Application nav -->
+    <div class="nav-application clearfix">
+        <a href="client-dashboard.php" class="btn btn-square text-sm active">
+            <span class="btn-inner--icon d-block"><i class="far fa-home fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Home</span>
+        </a>
+        <a href="client-subscriptions.php" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-project-diagram fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Subscriptions</span>
+        </a>
+        <a href="client-invoices.php" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-tasks fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Invoices</span>
+        </a>
+        <a href="client-erp-instance.php" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-columns fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">ERP Instace</span>
+        </a>
+        <a href="client-api-keys.php" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-cogs fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">API Keys</span>
+        </a>
+        <a href="client-logout.php" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-power-off fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Log Out</span>
+        </a>
+    </div>
+
+</div>

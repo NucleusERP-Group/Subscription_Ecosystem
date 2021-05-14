4b76aaa042bc79a00652611615fe145ff4d02453
MartMbithi <martdevelopers254@gmail.com>
2021-05-14 11:05:57 +0300

    Crafted Invoice Payment Logic

--- views/client-view-invoice.php
+++ views/client-view-invoice.php
@@ -28,28 +28,6 @@ client_login();
 /* Load Barcode Library And Composer */
 require_once('../vendor/autoload.php');
 $barcode = new \Com\Tecnick\Barcode\Barcode();
-/* Generate A QR Code To Verify The Transaction */
-$targetPath = "../public/barcodes/";
-if (!is_dir($targetPath)) {
-    mkdir($targetPath, 0777, true);
-}
-$code = $invoice->invoice_code;
-$created_at = date('d M Y g:ia', strtotime($invoice->created_at));
-$due = date_format($due_date, 'd M Y g:ia');
-$amount = $invoice->subscription_amt;
-$client_details = $invoice->client_name . " " . $invoice->client_email;
-$qrcodedata = "Hello $client_details, Invoice Code: $code, Created At : $created_at , Due On: $due, Amount Invoiced: Ksh $amount.";
-$bobj = $barcode->getBarcodeObj(
-    'QRCODE,H',
-    "{$qrcodedata}",
-    -4,
-    -4,
-    'black',
-    array(-2, -2, -2, -2)
-)->setBackgroundColor('white');
-$imageData = $bobj->getPngData();
-$timestamp = time();
-file_put_contents($targetPath . $timestamp . '.png', $imageData);
 /* Add Invoice Payment */
 
 require_once('../partials/dashboard_head.php');
@@ -168,6 +146,30 @@ require_once('../partials/dashboard_head.php');
                                     <br>
                                     <div class="text-center">
                                         <h6>Scan To Verify</h6>
+                                        <?php
+                                        /* Generate A QR Code To Verify The Transaction */
+                                        $targetPath = "../public/barcodes/";
+                                        if (!is_dir($targetPath)) {
+                                            mkdir($targetPath, 0777, true);
+                                        }
+                                        $code = $invoice->invoice_code;
+                                        $created_at = date('d M Y g:ia', strtotime($invoice->created_at));
+                                        $due = date_format($due_date, 'd M Y g:ia');
+                                        $amount = $invoice->subscription_amt;
+                                        $client_details = $invoice->client_name . " " . $invoice->client_email;
+                                        $qrcodedata = "Hello $client_details, Invoice Code: $code, Created At : $created_at , Due On: $due, Amount Invoiced: Ksh $amount.";
+                                        $bobj = $barcode->getBarcodeObj(
+                                            'QRCODE,H',
+                                            "{$qrcodedata}",
+                                            -4,
+                                            -4,
+                                            'black',
+                                            array(-2, -2, -2, -2)
+                                        )->setBackgroundColor('white');
+                                        $imageData = $bobj->getPngData();
+                                        $timestamp = time();
+                                        file_put_contents($targetPath . $timestamp . '.png', $imageData);
+                                        ?>
                                         <!-- Dump Generated Barcode To Image -->
                                         <img src="<?php echo $targetPath . $timestamp; ?>.png" width="150px" height="150px">
                                     </div>
@@ -201,10 +203,10 @@ require_once('../partials/dashboard_head.php');
                                     } ?>
                                     <!-- Add Payment Modal -->
                                     <div class="modal fade" id="pay-invoice-<?php echo $invoice->id; ?>">
-                                        <div class="modal-dialog  modal-lg">
-                                            <div class="modal-content">
+                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                            <div class=" modal-content">
                                                 <div class="modal-header">
-                                                    <h4 class="modal-title">Fill All Required Values </h4>
+                                                    <h4 class="modal-title">Pay Invoice : <?php echo $invoice->invoice_code; ?></h4>
                                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                         <span aria-hidden="true">&times;</span>
                                                     </button>
@@ -214,23 +216,6 @@ require_once('../partials/dashboard_head.php');
                                                     <form method="post" enctype="multipart/form-data" role="form">
                                                         <div class="card-body">
                                                             <div class="row">
-                                                                <div class="form-group col-md-4">
-                                                                    <!-- Hidden Values -->
-                                                                    <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
-                                                                    <input type="hidden" required name="client_id" value="<?php echo $client->id; ?>" class="form-control">
-                                                                    <input type="hidden" required name="client_name" value="<?php echo $client->name; ?>" class="form-control">
-                                                                    <input type="hidden" required name="client_email" value="<?php echo $client->email; ?>" class="form-control">
-                                                                    <input type="hidden" required name="trans_code" value="<?php echo $paycde; ?>" class="form-control">
-                                                                    <input type="hidden" required name="subscription_id" value="<?php echo $invoice->subscription_id; ?>" class="form-control">
-                                                                    <input type="hidden" required name="package_code" value="<?php echo $invoice->package_code; ?>" class="form-control">
-                                                                    <input type="hidden" required name="package_name" value="<?php echo $invoice->package_name; ?>" class="form-control">
-                                                                    <input type="hidden" required name="amount" value="<?php echo $invoice->subscription_amt; ?>" class="form-control">
-                                                                    <!-- Update On User Subscriptions -->
-                                                                    <input type="hidden" required name="payment_status" value="Paid" class="form-control">
-                                                                    <!-- Update On User Invoices -->
-                                                                    <input type="hidden" required name="status" value="Paid" class="form-control">
-
-                                                                </div>
                                                                 <div class="form-group col-md-12">
                                                                     <label for="">Credit / Debit Card Number</label>
                                                                     <select class="form-control" name="cc_number">
@@ -246,13 +231,29 @@ require_once('../partials/dashboard_head.php');
                                                                         <?php
                                                                         } ?>
                                                                     </select>
+                                                                    <!-- Hidden Values -->
+                                                                    <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
+                                                                    <input type="hidden" required name="client_id" value="<?php echo $client->id; ?>" class="form-control">
+                                                                    <input type="hidden" required name="client_name" value="<?php echo $client->name; ?>" class="form-control">
+                                                                    <input type="hidden" required name="client_email" value="<?php echo $client->email; ?>" class="form-control">
+                                                                    <input type="hidden" required name="trans_code" value="<?php echo $paycde; ?>" class="form-control">
+                                                                    <input type="hidden" required name="subscription_id" value="<?php echo $invoice->subscription_id; ?>" class="form-control">
+                                                                    <input type="hidden" required name="package_code" value="<?php echo $invoice->package_code; ?>" class="form-control">
+                                                                    <input type="hidden" required name="package_name" value="<?php echo $invoice->package_name; ?>" class="form-control">
+                                                                    <input type="hidden" required name="amount" value="<?php echo $invoice->subscription_amt; ?>" class="form-control">
+                                                                    <!-- Update On User Subscriptions -->
+                                                                    <input type="hidden" required name="payment_status" value="Paid" class="form-control">
+                                                                    <!-- Update On User Invoices -->
+                                                                    <input type="hidden" required name="status" value="Paid" class="form-control">
                                                                 </div>
-
+                                                                <div class="form-group col-md-12">
+                                                                    <label for="">Invoice Amount (Ksh)</label>
+                                                                    <input type="text" required name="amount" value="<?php echo $invoice->subscription_amt; ?>" class="form-control">
+                                                                </div>
                                                             </div>
-
                                                         </div>
                                                         <div class="card-footer text-right">
-                                                            <button type="submit" name="paySubscription" class="btn btn-primary">Add Module</button>
+                                                            <button type="submit" name="paySubscription" class="btn btn-primary">Pay Invoice</button>
                                                         </div>
                                                     </form>
                                                 </div>

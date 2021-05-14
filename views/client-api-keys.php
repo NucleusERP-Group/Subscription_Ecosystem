3a8581075f6d1ed5fc2d4367f40860c350cc70b5
MartMbithi <martdevelopers254@gmail.com>
2021-05-14 12:24:30 +0300

    Implemented ClientERP Instances Based On Subscibed Packages & API Keys

---
+++ views/client-api-keys.php
@@ -0,0 +1,152 @@
+<?php
+/*
+ * Created on Fri May 14 2021
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2021 MartDevelopers Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+session_start();
+require_once('../config/config.php');
+require_once('../config/checklogin.php');
+client_login();
+require_once('../partials/dashboard_head.php');
+?>
+
+<body class="application application-offset">
+
+    <!-- Application container -->
+    <div class="container-fluid container-application">
+        <!-- Sidenav -->
+        <?php require_once('../partials/dashboard_sidenav.php'); ?>
+        <!-- Content -->
+        <div class="main-content position-relative">
+            <!-- Main nav -->
+            <?php
+            require_once('../partials/dashboard_main_nav.php');
+            $id = $_SESSION['id'];
+            $email = $_SESSION['email'];
+            $ret = "SELECT * FROM `NucleusSAASERP_Users` WHERE id = '$id' OR email = '$email' ";
+            $stmt = $mysqli->prepare($ret);
+            $stmt->execute(); //ok
+            $res = $stmt->get_result();
+            while ($client = $res->fetch_object()) {
+            ?>
+                <!-- Page content -->
+                <div class="page-content">
+                    <!-- Page title -->
+                    <div class="page-title">
+                        <div class="row justify-content-between align-items-center">
+                            <div class="col-md-6 d-flex align-items-center justify-content-between justify-content-md-start mb-3 mb-md-0">
+                                <!-- Page title + Go Back button -->
+                                <div class="d-inline-block">
+                                    <h5 class="h4 d-inline-block font-weight-400 mb-0 text-white">NucleusSaaS ERP Instances API Keys</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Listing -->
+                    <div class="card">
+                        <!-- Card header -->
+                        <div class="card-header actions-toolbar border-0">
+                            <div class="actions-search" id="actions-search">
+                                <div class="input-group input-group-merge input-group-flush">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text bg-transparent"><i class="far fa-search"></i></span>
+                                    </div>
+                                    <input type="text" id="SearchInput" onkeyup="search()" class="form-control form-control-flush" placeholder="Type ...">
+                                    <div class="input-group-append">
+                                        <a href="#" class="input-group-text bg-transparent" data-action="search-close" data-target="#actions-search"><i class="far fa-times"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row justify-content-between align-items-center">
+
+                                <div class="col text-right">
+                                    <div class="actions"><a href="#" class="action-item mr-3" data-action="search-open" data-target="#actions-search"><i class="far fa-search"></i></a>
+                                        <div class="dropdown mr-3">
+                                            <a href="#" class="action-item" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                <i class="far fa-filter"></i>
+                                            </a>
+                                            <div class="dropdown-menu dropdown-menu-right">
+                                                <button class="dropdown-item" onclick="sortTable()">
+                                                    <i class="far fa-sort-alpha-down"></i>Sort
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Table -->
+                        <div class="table-responsive">
+                            <table id="DataTable" class="table align-items-center">
+                                <thead>
+                                    <tr>
+                                        <th scope="col" class="sort">API Key</th>
+                                        <th scope="col" class="sort">Key Status</th>
+                                        <th scope="col" class="sort">Key Details</th>
+                                        <th scope="col">Date Created</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $ret = "SELECT * FROM `NucleusSAASERP_APIKeys` ";
+                                    $stmt = $mysqli->prepare($ret);
+                                    $stmt->execute(); //ok
+                                    $res = $stmt->get_result();
+                                    while ($apiKeys = $res->fetch_object()) {
+                                    ?>
+                                        <tr>
+                                            <td>
+                                                <?php echo $apiKeys->api_key; ?>
+                                            </td>
+                                            <td>
+                                                <?php
+                                                if ($apiKeys->status = 'Active') {
+                                                    echo "<span class='badge badge-pill badge-success'>Active</span>";
+                                                } else {
+                                                    echo "<span class='badge badge-pill badge-danger'>Revoked</span>";
+                                                }
+                                                ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $apiKeys->details; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo date('d M Y g:ia', strtotime($apiKeys->created_at)); ?>
+                                            </td>
+                                        </tr>
+                                    <?php
+                                    } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <!-- Footer -->
+            <?php require_once('../partials/dashboard_footer.php');
+            } ?>
+        </div>
+    </div>
+    <!-- Scripts -->
+    <?php require_once('../partials/dashboard_scripts.php'); ?>
+</body>
+
+
+
+</html>
---
+++ views/client-erp-instance.php
@@ -0,0 +1,146 @@
+<?php
+/*
+ * Created on Fri May 14 2021
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2021 MartDevelopers Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+session_start();
+require_once('../config/config.php');
+require_once('../config/checklogin.php');
+client_login();
+require_once('../partials/dashboard_head.php');
+?>
+
+<body class="application application-offset">
+
+    <!-- Application container -->
+    <div class="container-fluid container-application">
+        <!-- Sidenav -->
+        <?php require_once('../partials/dashboard_sidenav.php'); ?>
+        <!-- Content -->
+        <div class="main-content position-relative">
+            <!-- Main nav -->
+            <?php
+            require_once('../partials/dashboard_main_nav.php');
+            $id = $_SESSION['id'];
+            $email = $_SESSION['email'];
+            $ret = "SELECT * FROM `NucleusSAASERP_Users` WHERE id = '$id' OR email = '$email' ";
+            $stmt = $mysqli->prepare($ret);
+            $stmt->execute(); //ok
+            $res = $stmt->get_result();
+            while ($client = $res->fetch_object()) {
+            ?>
+                <!-- Page content -->
+                <div class="page-content">
+                    <!-- Page title -->
+                    <div class="page-title">
+                        <div class="row justify-content-between align-items-center">
+                            <div class="col-md-6 d-flex align-items-center justify-content-between justify-content-md-start mb-3 mb-md-0">
+                                <!-- Page title + Go Back button -->
+                                <div class="d-inline-block">
+                                    <h5 class="h4 d-inline-block font-weight-400 mb-0 text-white">NucleusSaaS ERP Instances</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Listing -->
+                    <div class="card">
+                        <!-- Card header -->
+                        <div class="card-header actions-toolbar border-0">
+                            <div class="actions-search" id="actions-search">
+                                <div class="input-group input-group-merge input-group-flush">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text bg-transparent"><i class="far fa-search"></i></span>
+                                    </div>
+                                    <input type="text" id="SearchInput" onkeyup="search()" class="form-control form-control-flush" placeholder="Type Package Code ...">
+                                    <div class="input-group-append">
+                                        <a href="#" class="input-group-text bg-transparent" data-action="search-close" data-target="#actions-search"><i class="far fa-times"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row justify-content-between align-items-center">
+
+                                <div class="col text-right">
+                                    <div class="actions"><a href="#" class="action-item mr-3" data-action="search-open" data-target="#actions-search"><i class="far fa-search"></i></a>
+                                        <div class="dropdown mr-3">
+                                            <a href="#" class="action-item" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                <i class="far fa-filter"></i>
+                                            </a>
+                                            <div class="dropdown-menu dropdown-menu-right">
+                                                <button class="dropdown-item" onclick="sortTable()">
+                                                    <i class="far fa-sort-alpha-down"></i>Sort
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Table -->
+                        <div class="table-responsive">
+                            <table id="DataTable" class="table align-items-center">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Package Code</th>
+                                        <th scope="col" class="sort">Package Name</th>
+                                        <th scope="col">ERP Instance</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $ret = "SELECT * FROM `NucleusSAASERP_ERPInstances` WHERE client_id = '$id' OR client_email = '$email' ";
+                                    $stmt = $mysqli->prepare($ret);
+                                    $stmt->execute(); //ok
+                                    $res = $stmt->get_result();
+                                    while ($instances = $res->fetch_object()) {
+                                    ?>
+
+                                        <tr>
+                                            <td>
+                                                <?php echo $instances->package_code; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $instances->package_name; ?>
+                                            </td>
+                                            <td>
+                                                <a href="<?php echo $instances->instance_url; ?>" class="badge badge-pill badge-primary" target="_blank">
+                                                    <i class="fas fa-open"></i>
+                                                    Access ERP Instance
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    <?php
+                                    } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <!-- Footer -->
+            <?php require_once('../partials/dashboard_footer.php');
+            } ?>
+        </div>
+    </div>
+    <!-- Scripts -->
+    <?php require_once('../partials/dashboard_scripts.php'); ?>
+</body>
+
+
+
+</html>

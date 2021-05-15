109c64f2b1fe387c38a84d402b3fd9bda5de0061
MartMbithi <martdevelopers254@gmail.com>
2021-05-15 16:51:09 +0300

    Update & Delete Logic On Subscription Packages

--- views/admin-subscriptions.php
+++ views/admin-subscriptions.php
@@ -26,6 +26,84 @@ require_once('../config/checklogin.php');
 require_once('../config/codeGen.php');
 client_login();
 /* Add Package */
+if (isset($_POST['addPackage'])) {
+    //Error Handling and prevention of posting double entries
+    $error = 0;
+    if (isset($_POST['id']) && !empty($_POST['id'])) {
+        $id = mysqli_real_escape_string($mysqli, trim($_POST['id']));
+    } else {
+        $error = 1;
+        $err = "ID Cannot Be Empty";
+    }
+
+    if (isset($_POST['package_code']) && !empty($_POST['package_code'])) {
+        $package_code = mysqli_real_escape_string($mysqli, trim($_POST['package_code']));
+    } else {
+        $error = 1;
+        $err = "Package Code Cannot Be Empty";
+    }
+
+    if (isset($_POST['package_name']) && !empty($_POST['package_name'])) {
+        $package_name = mysqli_real_escape_string($mysqli, trim($_POST['package_name']));
+    } else {
+        $error = 1;
+        $err = "Package Name Cannot Be Empty";
+    }
+
+    if (isset($_POST['package_details']) && !empty($_POST['package_details'])) {
+        $package_details = mysqli_real_escape_string($mysqli, trim($_POST['package_details']));
+    } else {
+        $error = 1;
+        $err = "Package Details  Cannot Be Empty";
+    }
+
+    if (isset($_POST['package_monthly_price ']) && !empty($_POST['package_monthly_price '])) {
+        $package_monthly_price  = mysqli_real_escape_string($mysqli, trim($_POST['package_monthly_price ']));
+    } else {
+        $error = 1;
+        $err = "Packet Monthly Price  Cannot Be Empty";
+    }
+
+    if (isset($_POST['package_yearly_price']) && !empty($_POST['package_yearly_price'])) {
+        $package_yearly_price = mysqli_real_escape_string($mysqli, trim($_POST['package_yearly_price']));
+    } else {
+        $error = 1;
+        $err = "Package Yearly Price  Cannot Be Empty";
+    }
+
+    if (isset($_POST['package_status']) && !empty($_POST['package_status'])) {
+        $package_status = mysqli_real_escape_string($mysqli, trim($_POST['package_status']));
+    } else {
+        $error = 1;
+        $err = "Package Status  Cannot Be Empty";
+    }
+
+
+    if (!$error) {
+        /* Prevent Double Entries */
+        $sql = "SELECT * FROM  NucleusSAASERP_Packages WHERE   package_code = '$package_code'    ";
+        $res = mysqli_query($mysqli, $sql);
+        if (mysqli_num_rows($res) > 0) {
+            $row = mysqli_fetch_assoc($res);
+            if ($package_code == $row['package_code']) {
+                $err =  "A Subscription Package  With This Code:  $package_code Exists.";
+            }
+        } else {
+            /* No Error Or Duplicate */
+            $query = "INSERT INTO NucleusSAASERP_Packages  (id, package_code, package_name, package_details, package_monthly_price, package_yearly_price, package_status ) VALUES (?,?,?,?,?,?,?)";
+            $stmt = $mysqli->prepare($query);
+            $rc = $stmt->bind_param('sssssss', $id, $package_code, $package_name, $package_details, $package_monthly_price, $package_yearly_price, $package_status);
+            $stmt->execute();
+            if ($stmt) {
+                $success = "Subscription Package Added";
+            } else {
+                $info = "Please Try Again Or Try Later ";
+            }
+        }
+    }
+}
+
+/* Update Package */
 if (isset($_POST['PurchasePackage'])) {
     //Error Handling and prevention of posting double entries
     $error = 0;
@@ -77,35 +155,36 @@ if (isset($_POST['PurchasePackage'])) {
         $error = 1;
         $err = "Package Status  Cannot Be Empty";
     }
-    
+
 
     if (!$error) {
-        /* Prevent Double Entries */
-        $sql = "SELECT * FROM  NucleusSAASERP_Packages WHERE   package_code = '$package_code'    ";
-        $res = mysqli_query($mysqli, $sql);
-        if (mysqli_num_rows($res) > 0) {
-            $row = mysqli_fetch_assoc($res);
-            if ($package_code == $row['package_code']) {
-                $err =  "A Subscription Package  With This Code:  $package_code Exists.";
-            }
+        /* No Error Or Duplicate */
+        $query = "UPDATE NucleusSAASERP_Packages  SET package_code =?, package_name =?, package_details =?, package_monthly_price =?, package_yearly_price =?, package_status =? WHERE id = ?";
+        $stmt = $mysqli->prepare($query);
+        $rc = $stmt->bind_param('sssssss', $package_code, $package_name, $package_details, $package_monthly_price, $package_yearly_price, $package_status, $id);
+        $stmt->execute();
+        if ($stmt) {
+            $success = "Subscription Package Updated";
         } else {
-            /* No Error Or Duplicate */
-            $query = "INSERT INTO NucleusSAASERP_Packages  (id, package_code, package_name, package_details, package_monthly_price, package_yearly_price, package_status ) VALUES (?,?,?,?,?,?,?)";
-            $stmt = $mysqli->prepare($query);
-            $rc = $stmt->bind_param('sssssss', $id, $package_code, $package_name, $package_details, $package_monthly_price, $package_yearly_price, $package_status);
-            $stmt->execute();
-            if ($stmt) {
-                $success = "Subscription Package Added";
-            } else {
-                $info = "Please Try Again Or Try Later ";
-            }
+            $info = "Please Try Again Or Try Later ";
         }
     }
 }
 
-/* Update Package */
-
 /* Delete Package */
+if (isset($_GET['delete'])) {
+    $delete = $_GET['delete'];
+    $adn = "DELETE FROM NucleusSAASERP_Packages WHERE id=?";
+    $stmt = $mysqli->prepare($adn);
+    $stmt->bind_param('s', $delete);
+    $stmt->execute();
+    $stmt->close();
+    if ($stmt) {
+        $success = "Deleted" && header("refresh:1; url=admin-subscriptions.php");
+    } else {
+        $info = "Please Try Again Or Try Later";
+    }
+}
 
 require_once('../partials/dashboard_head.php');
 ?>

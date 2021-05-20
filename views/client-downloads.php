0d18ba2e9216b41ac43bd8253027024a7dd8bc14
MartMbithi <martdevelopers254@gmail.com>
2021-05-20 11:18:49 +0300

    Added Download On ERP Instances

--- views/client-downloads.php
+++ views/client-downloads.php
@@ -55,6 +55,9 @@ require_once('../partials/dashboard_head.php');
                                 <!-- Page title + Go Back button -->
                                 <div class="d-inline-block">
                                     <h5 class="h4 d-inline-block font-weight-400 mb-0 text-white">Request For A Customized Android, iOS, Linux And Windows Execuatbles.</h5>
+                                    <p class="d-inline-block font-weight-400 mb-0 text-white">
+                                        NucleusSaaS ERPs are available in Android, iOs applications, windows 32 and 64 bit desktop applications and linux (debian based distributions) applications.
+                                    </p>
                                 </div>
                             </div>
                         </div>
@@ -102,7 +105,81 @@ require_once('../partials/dashboard_head.php');
                                                 </a>
                                             </td>
                                             <!-- Request For A Download -->
-                                            
+                                            <div class="modal fade" id="dowloads-<?php echo $instances->id; ?>">
+                                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                                    <div class=" modal-content">
+                                                        <div class="modal-header">
+                                                            <h4 class="modal-title">Configure Your Download Flavour</h4>
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                <span aria-hidden="true">&times;</span>
+                                                            </button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <!-- Add Module Form -->
+                                                            <form method="post" enctype="multipart/form-data" role="form">
+                                                                <div class="card-body">
+                                                                    <div class="row">
+                                                                        <div class="form-group col-md-12">
+                                                                            <label for="">Desktop Application Platorm</label>
+                                                                            <select class="form-control" name="desktop_platform">
+                                                                                <option>Select Operating System</option>
+                                                                                <option>Windows Operating System</option>
+                                                                                <option>Linux Operating System</option>
+                                                                                <option>Mac Operating System</option>
+                                                                            </select>
+                                                                            <!-- Hidden Values -->
+                                                                            <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
+                                                                            <input type="hidden" required name="client_id" value="<?php echo $client->id; ?>" class="form-control">
+                                                                            <input type="hidden" required name="client_name" value="<?php echo $client->name; ?>" class="form-control">
+                                                                            <input type="hidden" required name="client_email" value="<?php echo $client->email; ?>" class="form-control">
+                                                                            <input type="hidden" required name="trans_code" value="<?php echo $paycode; ?>" class="form-control">
+                                                                            <input type="hidden" required name="subscription_id" value="<?php echo $invoice->subscription_id; ?>" class="form-control">
+                                                                            <input type="hidden" required name="subscription_code" value="<?php echo $invoice->subscription_code; ?>" class="form-control">
+                                                                            <input type="hidden" required name="package_code" value="<?php echo $invoice->package_code; ?>" class="form-control">
+                                                                            <input type="hidden" required name="package_name" value="<?php echo $invoice->package_name; ?>" class="form-control">
+                                                                            <input type="hidden" required name="amount" value="<?php echo $invoice->subscription_amt; ?>" class="form-control">
+                                                                            <!-- Update On User Subscriptions -->
+                                                                            <input type="hidden" required name="payment_status" value="Paid" class="form-control">
+                                                                            <!-- Update On User Invoices -->
+                                                                            <input type="hidden" required name="status" value="Paid" class="form-control">
+                                                                            <input type="hidden" required name="invoice_id" value="<?php echo $invoice->id; ?>" class="form-control">
+                                                                            <!-- Notification Details -->
+                                                                            <input type="hidden" name="notification_from" value="Invoice Payment">
+                                                                            <input type="hidden" name="notification_details" value="Hello, <?php echo $client->name; ?>. You Have Successfully Paid Invoiced Subscription For This Package: <?php echo $invoice->package_code . " " . $invoice->package_name; ?>">
+                                                                            <!-- Mail To Client -->
+                                                                            <input type="hidden" name="subject" value="Invoice #:<?php echo $invoice->invoice_code; ?> Payment">
+                                                                            <input type="hidden" name="message" value="Hello, <?php echo $client->name; ?>, I hope you’re well!. You have successfully paid Ksh <?php echo $invoice->subscription_amt; ?> for  subscribed package :<b><?php echo $invoice->package_code . " " . $invoice->package_name; ?></b>.
+                                                                    Kindly proceed to view your attached paid invoice on Invoices Tab on your dashboard.<br>
+                                                                    Don’t hesitate to reach out if you have any questions.<br><br><br><br><br>
+                                                                    Kind Regards,<br>
+                                                                    <b>NucleusSaaS ERP Group</b><br>
+                                                                    <i>
+                                                                        Deploy your business operations and services on our fully redundant, 
+                                                                        high performance Software As Service Enterprise Resource Planning platform
+                                                                        and benefit from its high reliability, security and enterprise feature set.
+                                                                    </i>
+                                                                    ">
+                                                                        </div>
+                                                                        <div class="form-group col-md-12">
+                                                                            <label for="">Mobile Application Platform</label>
+                                                                            <select class="form-control" name="mobile_platform">
+                                                                                <option>Android </option>
+                                                                                <option>iOs</option>
+                                                                            </select>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="card-footer text-right">
+                                                                    <button type="submit" name="requestForDownload" class="btn btn-primary">Submit</button>
+                                                                </div>
+                                                            </form>
+                                                        </div>
+                                                        <div class="modal-footer justify-content-between">
+                                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                             <!-- End Request -->
                                         </tr>
                                     <?php
